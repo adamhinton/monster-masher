76fa4b5646d2de1a309bea33d90ad78196d2ef3b
adamhinton <adam.d.hinton@gmail.com>
2026-04-26 22:32:13 -0400

    Add logging when /health endpoint is hit

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
-
 from django.http import JsonResponse
 from django.views.decorators.http import require_GET
+import sentry_sdk
 
 
 @require_GET
 def health(request):
+    sentry_sdk.capture_message(
+        f"Health endpoint hit: {request.method} {request.path}",
+        level="info",
+    )
     return JsonResponse({"status": "ok"})
