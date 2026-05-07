1c4b8bdaca6985c20c63c01fa6cb05d367be09e4
adamhinton <adam.d.hinton@gmail.com>
2026-05-07 00:07:11 -0400

    Fix import issue

--- frontend/src/app/error.tsx
+++ frontend/src/app/error.tsx
@@ -3,7 +3,7 @@
 import { useEffect } from "react";
 import * as Sentry from "@sentry/nextjs";
 import { Button } from "@/components/ui/button";
-import { PageContainer } from "@/components/layout/page-container";
+import { PageContainer } from "@/components/layout/PageContainer";
 
 export default function Error({
 	error,
